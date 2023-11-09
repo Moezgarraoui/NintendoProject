8667cdb78700e0d0c8e59a222cddf3f3b0f14303
Moezgarraoui <garraouimouez@gmail.com>
2023-11-09 11:10:23 +0100

    5objetsjeu

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -2,16 +2,14 @@ package test;
 
 
 import java.util.List;
-import model.Client;
-import model.Adresse;
-import model.Boutique;
 
+import model.Client;
+import model.Jeu ; 
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		List<String> achat = null; 
 		achat.add("pomme"); 
 		achat.add("ballon"); 
@@ -19,11 +17,13 @@ public class Test {
 
 		Client c1 = new Client("Mbappé", "kylian", achat); 
 		Client c2 = new Client("Gall", "France", achat); 
+		
+		Jeu jeu1 = new Jeu(null, null, null);
+        Jeu jeu2 = new Jeu(null, null, null);
+        Jeu jeu3 = new Jeu(null, null, null);
+        Jeu jeu4 = new Jeu(null, null, null);
+        Jeu jeu5 = new Jeu(null, null, null);
+	}}
 
 
-		Adresse a = new Adresse(2, "Rue du Chateaux", "Hyrule");
-		Boutique b = new Boutique("Carrefour", a);
 
-	}
-
-}
