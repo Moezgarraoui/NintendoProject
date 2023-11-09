b103ff345d22760d743eb79baa29a86f3a046c00
ricardoSA54 <ricardosa54@gmail.com>
2023-11-09 11:00:42 +0100

    added boutique

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,13 +1,17 @@
 package test;
 
-import java.util.List;
 
+import java.util.List;
 import model.Client;
+import model.Adresse;
+import model.Boutique;
+
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		List<String> achat = null; 
 		achat.add("pomme"); 
 		achat.add("ballon"); 
@@ -15,6 +19,10 @@ public class Test {
 
 		Client c1 = new Client("Mbappé", "kylian", achat); 
 		Client c2 = new Client("Gall", "France", achat); 
+
+
+		Adresse a = new Adresse(2, "Rue du Chateaux", "Hyrule");
+		Boutique b = new Boutique("Carrefour", a);
 
 	}
 
