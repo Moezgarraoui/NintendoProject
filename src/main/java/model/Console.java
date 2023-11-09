e2a84fe05a43b304689be26543cbba676b66c570
ricardoSA54 <ricardosa54@gmail.com>
2023-11-09 11:11:08 +0100

    added prix and date to console

--- src/main/java/model/Console.java
+++ src/main/java/model/Console.java
@@ -1,25 +1,41 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Console {
 
 	private String nom;
-
-	public Console(String nom) {
+	private int prix;
+	private LocalDate date;
+	public Console(String nom, int prix, LocalDate date) {
 		this.nom = nom;
+		this.prix = prix;
+		this.date = date;
 	}
-
 	public String getNom() {
 		return nom;
 	}
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
+	public int getPrix() {
+		return prix;
+	}
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", date=" + date + "]";
 	}
+
+	
 	
 	
 }
